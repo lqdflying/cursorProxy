f40a73fc1f85523e19bdf9946d84c4741fe520ed
lqdflying <lqdflying@gmail.com>
2026-05-09 01:03:53 +0800

    Revert "Fix Azure custom tool chaining"
    This reverts commit e04d3773cde021dee614f34e357b39e62ca3ef7e.

--- api/proxy.js
+++ api/proxy.js
@@ -40,7 +40,7 @@ import {
 import { convertImagesToText } from "./vision-bridge.js";
 
 const DEBUG = process.env.DEBUG === "true";
-const AZURE_OPENAI_RESPONSE_CACHE_VERSION = "v5";
+const AZURE_OPENAI_RESPONSE_CACHE_VERSION = "v4";
 let proxyAuthWarningLogged = false;
 
 const PROVIDERS = {
